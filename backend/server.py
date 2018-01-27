9c68b155f4e676a9fd9c0702af34d3376fc6108c
Ben Chapman-Kish <ben.chapmankish@gmail.com>
2018-01-27 18:14:43 -0500

    Add user login and auth to flask server

--- backend/server.py
+++ backend/server.py
@@ -1,15 +1,85 @@
-from flask import Flask
-from flask import request
+import flask
+import flask_login
 import json
 
-app = Flask(__name__)
+app = flask.Flask(__name__)
+app.secret_key = 'DecorAssist secret key ;)'
 
 @app.route('/', methods=['GET','POST'])
 def main():
-    data = request.data
+    data = flask.request.data
     print(data)
-    if request.method == 'POST':
+    if flask.request.method == 'POST':
         print()
         return json.dumps({'success':True})
-    elif request.method == 'GET':
+    elif flask.request.method == 'GET':
         return json.dumps({'test':'test'})
+
+
+
+# Mock database:
+users = {  'steven': {'password': 'mypass'},
+        'big_poppa': {'password': 'daddy'}}
+
+login_manager = flask_login.LoginManager()
+
+login_manager.init_app(app)
+
+class User(flask_login.UserMixin):
+    pass
+
+@login_manager.user_loader
+def user_loader(username):
+    if username not in users:
+        return
+
+    user = User()
+    user.id = username
+    return user
+
+@login_manager.request_loader
+def request_loader(request):
+    username = request.form.get('username')
+    if username not in users:
+        return
+
+    user = User()
+    user.id = username
+
+    user.is_authenticated = request.form['password'] == users[username]['password']
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if flask.request.method == 'GET':
+        return '''
+               <form action='login' method='POST'>
+                <input type='text' name='username' id='username' placeholder='username'/>
+                <input type='password' name='password' id='password' placeholder='password'/>
+                <input type='submit' name='submit'/>
+               </form>
+               '''
+
+    username = flask.request.form['username']
+    if flask.request.form['password'] == users[username]['password']:
+        user = User()
+        user.id = username
+        flask_login.login_user(user)
+        return flask.redirect(flask.url_for('protected'))
+
+    return 'Bad login'
+
+
+@app.route('/protected')
+@flask_login.login_required
+def protected():
+    return 'Logged in as: ' + flask_login.current_user.id
+
+@app.route('/logout')
+def logout():
+    flask_login.logout_user()
+    return 'Logged out'
+
+@login_manager.unauthorized_handler
+def unauthorized_handler():
+    return 'Unauthorized'
