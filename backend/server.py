f6294989faa455df0813e369d16b3687e48bc626
Erick Leon <eri.lewo@hotmail.com>
2018-01-27 17:42:40 -0500

    =post,get request on root url

--- backend/server.py
+++ backend/server.py
@@ -1,7 +1,15 @@
 from flask import Flask
+from flask import request
 import json
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return json.dumps({'test':'test'})
+@app.route('/', methods=['GET','POST'])
+def main():
+    data = request.data
+    print(data)
+    if request.method == 'POST':
+        print()
+        return json.dumps({'success':True})
+    elif request.method == 'GET':
+        return json.dumps({'test':'test'})
